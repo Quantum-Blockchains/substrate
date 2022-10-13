10ddb68eb0c256f35d1343db013e363c095b2f09
kostiask <56359519+kostiask@users.noreply.github.com>
2022-10-13 10:27:15 +0200

    Update lib.rs

--- client/network/src/lib.rs
+++ client/network/src/lib.rs
@@ -256,7 +256,7 @@ pub mod config;
 pub mod network_state;
 
 #[doc(inline)]
-pub use libp2p::{multiaddr, Multiaddr, PeerId};
+pub use libp2p::{multiaddr, Multiaddr, PeerId, pnet::PreSharedKey};
 pub use protocol::PeerInfo;
 pub use sc_network_common::{
 	protocol::{
