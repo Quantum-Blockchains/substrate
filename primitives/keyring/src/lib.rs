83fb382475f306370c92e3d4aa86879f48800aa8
Łukasz Kujawski <leon@wildland.io>
2022-12-21 15:14:01 +0100

    JEQB-163: add dilithium2 keyring

--- primitives/keyring/src/lib.rs
+++ primitives/keyring/src/lib.rs
@@ -23,6 +23,9 @@ pub mod sr25519;
 /// Test account crypto for ed25519.
 pub mod ed25519;
 
+/// Test account crypto for dilithium2.
+pub mod dilithium2;
+
 /// Convenience export: Sr25519's Keyring is exposed as `AccountKeyring`,
 /// since it tends to be used for accounts (although it may also be used
 /// by authorities).
